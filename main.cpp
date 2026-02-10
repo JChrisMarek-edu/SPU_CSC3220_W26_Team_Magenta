030401944ebfcaea8951ebd5324607e24fe2387e
CraftyCreeper64 <nateb7796@outlook.com>
2026-02-09 18:22:17 -0800

    Updated json file + maybe broke stuff
    I have messed with the file so it compiles for me now, i have no idea if it will work easily for you guys. also, i don't really know too much about our project's end goal, so i'll leave it for later for us to discuss.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+cout<<"Welcome to OK Results."<<endl;
+string input = "";
+
+while (!(input == "q" || input == "quit")){
+cin>>input;
+
+if (input == "help" || input == "/help"){
+    //help command
+}
+
+
+
+}
+
+
+
+}
