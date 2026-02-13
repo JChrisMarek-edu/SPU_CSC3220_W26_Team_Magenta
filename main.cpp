604f5667b4210fd16f7748c20dc736ff3dc8a055
JChrisMarek-edu <marekj2@spu.edu>
2026-02-13 10:08:49 -0800

    Update main.cpp
    Added structs necessary for basic operations

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
+struct teams {
+    int teamID;
+    string teamName;
+}
+
+struct employee {
+    int employeeID;
+    string fName;
+    string lName;
+    string email;
+    string role;
+    string hireDate; //change to date object
+    int teamID;
+}
+
+struct objective {
+    int objectiveID;
+    string title;
+    string description;
+    string startDate;
+    string endDate;
+    string status;
+    int teamID;
+    int ownerEmployeeID;
+}
 
 int main(){
 
